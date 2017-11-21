3ff10cd7742f22d52d9b5ffd9f7d9a120b9f3764
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-11-21 07:34:07 -0800

    Norme time

--- includes/rtv1.h
+++ includes/rtv1.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rtv1.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/21 07:28:01 by jaleman           #+#    #+#             */
+/*   Updated: 2017/11/21 07:28:02 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef RTV1_H
 # define RTV1_H
 
@@ -21,22 +33,22 @@
 ** Macros used for the window.
 */
 
-# define WIN_WIDTH    (1280)
-# define WIN_HEIGHT    (720)
-# define WIN_TITLE    "RTv1 - Pepa Edition"
+# define WIN_WIDTH		(1280)
+# define WIN_HEIGHT		(720)
+# define WIN_TITLE		"RTv1 - Pepa Edition"
 
 /*
 ** Redefine some of the keys from the keyboard (not following a standard).
 */
 
 # undef KEY_CONTROL
-# define KEY_CONTROL  (0X100)
+# define KEY_CONTROL	(0X100)
 # undef KEY_SHIFT
-# define KEY_SHIFT    (0X101)
+# define KEY_SHIFT		(0X101)
 # undef KEY_COMMAND
-# define KEY_COMMAND  (0X103)
+# define KEY_COMMAND	(0X103)
 # undef KEY_OPTION
-# define KEY_OPTION    (0X105)
+# define KEY_OPTION		(0X105)
 
 /*
 ** RTv1 Structure
@@ -53,31 +65,31 @@
 ** @t_rtv1 : Nested structure that calls all the other structures. ;)
 */
 
-typedef struct  s_mlx
+typedef struct	s_mlx
 {
-  void   *init;
-  void   *win;
-  void   *img;
-}               t_mlx;
+	void	*init;
+	void	*win;
+	void	*img;
+}				t_mlx;
 
-typedef struct  s_img
+typedef struct	s_img
 {
-  int    bpp;
-  int    size;
-  int    endian;
-  int    *data;
-}               t_img;
+	int		bpp;
+	int		size;
+	int		endian;
+	int		*data;
+}				t_img;
 
-typedef struct  s_rtv1
+typedef struct	s_rtv1
 {
-  t_mlx  mlx;
-  t_img  image;
-}               t_rtv1;
+	t_mlx	mlx;
+	t_img	image;
+}				t_rtv1;
 
 /*
 ** Function prototypes.
 */
 
-void      set_hooks(t_rtv1 *rtv1);
+void			set_hooks(t_rtv1 *rtv1);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,57 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/21 07:27:52 by jaleman           #+#    #+#             */
+/*   Updated: 2017/11/21 07:27:53 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "rtv1.h"
 
 /*
 ** Prints the usage message if the number of arguments are invalid.
 */
 
-static void print_usage(char *progname)
+static void		print_usage(char *progname)
 {
-    ft_putstr("usage: ");
-    ft_putstr(progname);
-    ft_putendl(" file.rtv1");
-    return ;
+	ft_putstr("usage: ");
+	ft_putstr(progname);
+	ft_putendl(" file.rtv1");
+	return ;
 }
 
 /*
 ** Init the rtv1 nested structure values (mlx, image).
 */
 
-static t_rtv1 *init_values(void)
+static t_rtv1	*init_values(void)
 {
-    t_rtv1  *rtv1;
+	t_rtv1		*rtv1;
 
-    rtv1 = (t_rtv1 *)malloc(sizeof(t_rtv1));
-    if (rtv1 == NULL)
-        ft_puterror("Memory Allocation failed! :O", 1);
-    rtv1->mlx.init = mlx_init();
-    rtv1->mlx.img = mlx_new_image(rtv1->mlx.init, WIN_WIDTH, WIN_HEIGHT);
-    rtv1->mlx.win = mlx_new_window(rtv1->mlx.init, WIN_WIDTH, WIN_HEIGHT, \
-                                    WIN_TITLE);\
-    rtv1->image.data = (int *)mlx_get_data_addr(rtv1->mlx.img, \
-                                                &rtv1->image.bpp, \
-                                                &rtv1->image.size, \
-                                                &rtv1->image.endian);
-    return (rtv1);
+	rtv1 = (t_rtv1 *)malloc(sizeof(t_rtv1));
+	if (rtv1 == NULL)
+		ft_puterror("Memory Allocation failed! :O", 1);
+	rtv1->mlx.init = mlx_init();
+	rtv1->mlx.img = mlx_new_image(rtv1->mlx.init, WIN_WIDTH, WIN_HEIGHT);
+	rtv1->mlx.win = mlx_new_window(rtv1->mlx.init, WIN_WIDTH, WIN_HEIGHT, \
+									WIN_TITLE);\
+	rtv1->image.data = (int *)mlx_get_data_addr(rtv1->mlx.img, \
+												&rtv1->image.bpp, \
+												&rtv1->image.size, \
+												&rtv1->image.endian);
+	return (rtv1);
 }
 
 /*
 ** Let's rock and roll! (∩｀-´)⊃━☆ﾟ.*･｡ﾟ
 */
 
-int   main(int argc, char *argv[])
+int				main(int argc, char *argv[])
 {
-    t_rtv1  *rtv1;
+	t_rtv1		*rtv1;
 
-    if (argc != 2)
-        print_usage(argv[0]);
-    else
-    {
-        rtv1 = init_values();
-        set_hooks(rtv1);
-        // check for file extension (.rtv1)
-        // save data to a 2d array
-        // read from cli
-        mlx_loop(rtv1->mlx.init);
-    }
-    return (0);
+	if (argc != 2)
+		print_usage(argv[0]);
+	else
+	{
+		rtv1 = init_values();
+		set_hooks(rtv1);
+		// check for file extension (.rtv1)
+		// save data to a 2d array
+		// read from cli
+		mlx_loop(rtv1->mlx.init);
+	}
+	return (0);
 }
