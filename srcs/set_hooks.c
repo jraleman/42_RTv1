bc6b264323a7e1e4b55fbcdf9647baad44252ed7
jraleman <jraleman@bendiburg.com>
2017-11-21 04:20:43 -0800

    Added key press and release hooks

--- srcs/set_hooks.c
+++ srcs/set_hooks.c
@@ -5,10 +5,34 @@
 ** frame thingy from the mac window.
 */
 
-static int	exit_hook(t_rtv1 *rtv1)
+static int  exit_hook(t_rtv1 *rtv1)
 {
     mlx_destroy_window(rtv1->mlx.init, rtv1->mlx.win);
     exit(0);
+    return (0);
+}
+
+/*
+** Hook used when a key is released, reset the value of a key back to zero.
+*/
+
+static int  key_release_hook(int key, t_rtv1 *rtv1)
+{
+  if (key == KEY_ESCAPE)
+      exit_hook(rtv1);
+  return (0);
+}
+
+/*
+** Hook used when a key is released, updates the value of a key to one,
+** indicating that it's being pressed.
+*/
+
+static int  key_press_hook(int key, t_rtv1 *rtv1)
+{
+  if (key == KEY_ESCAPE)
+      exit_hook(rtv1);
+  return (0);
 }
 
 /*
@@ -17,6 +41,8 @@ static int	exit_hook(t_rtv1 *rtv1)
 
 void      set_hooks(t_rtv1 *rtv1)
 {
+    mlx_hook(rtv1->mlx.win, 2, 0, key_press_hook, rtv1);
+    mlx_hook(rtv1->mlx.win, 3, 0, key_release_hook, rtv1);
     mlx_hook(rtv1->mlx.win, 17, 0, exit_hook, rtv1);
     return ;
 }
