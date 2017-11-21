089a47d91f458a9d6a6fd6e09efd09db0b719398
jraleman <jraleman@bendiburg.com>
2017-11-21 04:10:49 -0800

    Added rtv1 struct, rtv1 init_values function

--- includes/rtv1.h
+++ includes/rtv1.h
@@ -1,7 +1,62 @@
 #ifndef RTV1_H
 # define RTV1_H
 
-# include "libft.h"
+/*
+** Required header files.
+*/
+
+# include <math.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+/*
+** User defined header files.
+*/
+
 # include "mlx.h"
+# include "libft.h"
+# include "keys.h"
+
+/*
+** Macros used for the window.
+*/
+
+# define WIN_WIDTH    (1280)
+# define WIN_HEIGHT    (720)
+# define WIN_TITLE    "RTv1 - Pepa Edition"
+
+/*
+** Redefine some of the keys from the keyboard (not following a standard).
+*/
+
+# undef KEY_CONTROL
+# define KEY_CONTROL  (0X100)
+# undef KEY_SHIFT
+# define KEY_SHIFT    (0X101)
+# undef KEY_COMMAND
+# define KEY_COMMAND  (0X103)
+# undef KEY_OPTION
+# define KEY_OPTION    (0X105)
+
+typedef struct  s_mlx
+{
+  void   *init;
+  void   *win;
+  void   *img;
+}               t_mlx;
+
+typedef struct  s_img
+{
+  int    bpp;
+  int    size;
+  int    endian;
+  int    *data;
+}               t_img;
+
+typedef struct  s_rtv1
+{
+  t_mlx  mlx;
+  t_img  image;
+}               t_rtv1;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,5 +1,9 @@
 #include "rtv1.h"
 
+/*
+** Prints the usage message if the number of arguments are invalid.
+*/
+
 static void print_usage(char *progname)
 {
     ft_putstr("usage: ");
@@ -8,15 +12,46 @@ static void print_usage(char *progname)
     return ;
 }
 
+/*
+** Init the rtv1 nested structure values (mlx, image).
+*/
+
+static t_rtv1 *init_values(void)
+{
+    t_rtv1  *rtv1;
+
+    rtv1 = (t_rtv1 *)malloc(sizeof(t_rtv1));
+    if (rtv1 == NULL)
+        ft_puterror("Memory Allocation failed! :O", 1);
+    rtv1->mlx.init = mlx_init();
+    rtv1->mlx.img = mlx_new_image(rtv1->mlx.init, WIN_WIDTH, WIN_HEIGHT);
+    rtv1->mlx.win = mlx_new_window(rtv1->mlx.init, WIN_WIDTH, WIN_HEIGHT, \
+                                    WIN_TITLE);\
+    rtv1->image.data = (int *)mlx_get_data_addr(rtv1->mlx.img, \
+                                                &rtv1->image.bpp, \
+                                                &rtv1->image.size, \
+                                                &rtv1->image.endian);
+    return (rtv1);
+}
+
+/*
+** Let's rock and roll! (∩｀-´)⊃━☆ﾟ.*･｡ﾟ
+*/
+
 int   main(int argc, char *argv[])
 {
+    t_rtv1  *rtv1;
+
     if (argc != 2)
         print_usage(argv[0]);
     else
     {
+        rtv1 = init_values();
+        //set_hooks(rtv1);
         // check for file extension (.rtv1)
         // save data to a 2d array
         // read from cli
+        mlx_loop(rtv1->mlx.init);
     }
     return (0);
 }
