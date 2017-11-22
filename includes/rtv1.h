ceb588431aae6970d6e9e9966989b05525da6da3
jraleman <jraleman@bendiburg.com>
2017-11-22 01:59:39 -0800

    Added read_file() function

--- includes/rtv1.h
+++ includes/rtv1.h
@@ -18,6 +18,7 @@
 */
 
 # include <math.h>
+# include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
 
--- srcs/get_data.c
+++ srcs/get_data.c
@@ -12,10 +12,30 @@
 
 #include "rtv1.h"
 
+/*
+** Read the file content.
+*/
+
+static void read_file(t_rtv1 *rtv1, char *filename)
+{
+  int   fd;
+
+	rtv1 = NULL;
+  if ((fd = open(filename, O_RDONLY)) < 0)
+      ft_puterror("Error opening file, does it exist?", 3);
+  //while (get_next_line())
+  return ;
+}
+
+/*
+** Get the data from the file.
+*/
+
 t_rtv1	*get_data(t_rtv1 *rtv1, char *filename)
 {
 	rtv1 = NULL;
   if (ft_file_extension(filename, ".rt") == 0)
        ft_puterror("Check the file extension! :O", 2);
+  read_file(rtv1, filename);
 	return (rtv1);
 }
